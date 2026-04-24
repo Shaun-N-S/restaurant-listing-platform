5932927858b299b804b8a33731f6b03cb7507ca7
Shaun-N-S <shaunns7025@gmail.com>
2026-04-24 09:07:27 +0530

    Remove default database configuration values for production readiness

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "1234",
-    database: process.env.DB_NAME || "restaurant_db",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
